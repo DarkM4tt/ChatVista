b2c14e617cbeea1ce199e3e01abaf53f6e18b332
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2024-03-17 23:47:48 +0530

    Added group chat functionality.

--- frontend/src/pages/Chatpage.js
+++ frontend/src/pages/Chatpage.js
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/layout";
 import { useState } from "react";
-// import Chatbox from "../components/Chatbox";
+import Chatbox from "../components/Chatbox";
 import MyChats from "../components/MyChats";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import { ChatState } from "../context/ChatProvider";
@@ -14,9 +14,9 @@ const Chatpage = () => {
       {user && <SideDrawer />}
       <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
         {user && <MyChats fetchAgain={fetchAgain} />}
-        {/* {user && (
+        {user && (
           <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-        )} */}
+        )}
       </Box>
     </div>
   );
